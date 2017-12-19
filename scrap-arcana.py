aed239dddb4d0c5eaa4fae7236bd8897ddcd6903
Brihi <brihi16142@iiitd.ac.in>
2017-12-19 16:10:58 +0530

    Added scraper for the arcana webpage

---
+++ scrap-arcana.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import requests
+
+fhandle = open("arcana-occupations.txt","a")
+
+r=requests.get("http://arcana.wikidot.com/list-of-all-occupations")
+data=r.text
+
+soup = BeautifulSoup(data, "html.parser")
+
+for item in soup.find_all("a",href = True, class_="newpage"):
+
+    occupation = (str(item['href']).lstrip("/"))+"\n"
+    
+    fhandle.write(occupation)
