4d48cbda2ffb12dfb3686a128519173a6c06a4c0
adsrc <aditya16217@iiitd.ac.in>
2017-12-20 17:59:48 +0530

    Ran the nltk engine for Arcana occupations

--- test-nltk.py
+++ test-nltk.py
@@ -9,16 +9,19 @@ def differentiator(word):
     female_score = 0
 
     try:
+
         word = word.lower()
-        if word.find(" ") or word.find("-"):
+        if word.find(" "):
             tmp = word.split()
             word = "_".join(tmp)
-            print(word)
+
+
+        if "-" in word:
+            tmp = word.split("-")
+            word = "_".join(tmp)
+
         txt = wn.synsets(word)
-        #print(word.hyponyms())
-        # #print(txt)
-        # s1 = "zoologist"
-        # print(s1 + " " +word + " " + str(s1==word))
+
         defn = txt[0].definition()
         tokenized_defn = defn.split()
 
@@ -28,7 +31,6 @@ def differentiator(word):
 
         for token in MALE_SPECIFIC:
             if(token in tokenized_defn):
-                #print(defn.find(token))
                 male_score+=1
                 return
 
@@ -48,8 +50,7 @@ def differentiator(word):
 
     except Exception as err:
 
-        #print("Definition not found for " + word)
-        print("")
+        print("Definition not found for " + word)
 
 def corpus_generator(filename):
 
@@ -72,4 +73,4 @@ corpus_generator("arcana-occupations.txt")
 #     txt = txt.lower()
 #     fw.write(line)
 #differentiator("Music-teacher")
-#print(wn.synsets("music_teacher"))
+#print(wn.synsets("aerospace_engineer")[0].definition())
