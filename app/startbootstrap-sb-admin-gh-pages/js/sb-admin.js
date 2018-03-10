f7dabe2e94a6f2409fcc45c3acd5e75f23fd49e9
adsrc <aditya16217@iiitd.ac.in>
2018-03-11 02:15:45 +0530

    Started with UI

--- app/startbootstrap-sb-admin-gh-pages/js/sb-admin.js
+++ app/startbootstrap-sb-admin-gh-pages/js/sb-admin.js
@@ -1,3 +1,32 @@
+$("#flat-slider")
+    .slider({
+        max: 50,
+        min: 0,
+        range: true,
+        values: [15, 35],
+        
+    })
+    .slider("pips", {
+        first: "pip",
+        last: "pip",
+        rest: "label"
+    });
+                    
+$("#flat-slider-vertical-1, #flat-slider-vertical-2, #flat-slider-vertical-3")
+    .slider({
+        max: 25,
+        min: 0,
+        range: "min",
+        value: 20,
+        orientation: "vertical"
+    })
+    .slider("pips", {
+        first: "pip",
+        last: "pip",
+        
+    })
+    .slider("float");
+
 (function($) {
   "use strict"; // Start of use strict
   // Configure tooltips for collapsed side navigation
