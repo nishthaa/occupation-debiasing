e0429ba03af34eba593c1a7462a500178b12c2b5
Brihi <brihi16142@iiitd.ac.in>
2018-03-14 01:09:50 +0530

    Completed collection script

--- example-creation/generateExamples_new.py
+++ example-creation/generateExamples_new.py
@@ -21,15 +21,44 @@ def generate_query(occupation,gender):
     """
     return query
 
-query = generate_query("Painter","female")
-sparql.setQuery(query)
-sparql.setReturnFormat(CSV)
-results = sparql.query().convert().decode("utf-8")
-print(results)
-# print(results["results"]["bindings"][:5])
+def generate_result(occupation, gender):
+    query = generate_query(occupation, gender)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(CSV)
+    results = sparql.query().convert().decode("utf-8")
+    results = results[results.index('\n')+1:]
+    results = results.replace("\n",";")
+    results = results.replace("\"","")
+    return results
 
-    # SELECT *
-    # WHERE {
-    #     ?person rdf:type yago:Chemist109913824 .
-    #     ?person foaf:gender "male"@en
-    # }
+
+fh = open("batch-1.txt","r")
+fw = open("examples-new.txt",'a')
+fe = open("not-found.txt",'a')
+count = 1
+for line in fh:
+    occ = line.strip().capitalize()
+    try:
+        female_examples = generate_result(occ,"female")
+        male_examples = generate_result(occ, "male")
+        if female_examples=="" and male_examples=="":
+            """fe.write("No examples found"+" "+occ+"\n")
+            print(str(count)+" not found")
+            count+=1
+            continue"""
+            raise Exception("No examples found")
+        fw.write(occ + "|" + male_examples + "|" + female_examples + "\n")
+        print(str(count)+" found")
+        count+=1
+    except Exception as err:
+        fe.write(str(err)+" "+occ+"\n")
+        print(str(count)+" not found")
+        count+=1
+
+
+fw.close()
+fh.close()
+fe.close()
+
+
+#print(generate_result("Painter","female"))
