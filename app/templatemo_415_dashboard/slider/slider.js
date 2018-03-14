cbc1de93953ba48e6ff6775f6f16172bb34459c9
Brihi <brihi16142@iiitd.ac.in>
2018-03-15 03:19:31 +0530

    Text box for slider added

--- app/templatemo_415_dashboard/slider/slider.js
+++ app/templatemo_415_dashboard/slider/slider.js
@@ -14,5 +14,77 @@ $(document).ready( function() {
         rest: "label"
     });
 
+    var $slider = $("#flat-slider"),
+        $input = $("#from")
+          min = 0,
+          max = 50;
+
+      $input.on("change", function(e) {
+      
+        var num = parseFloat( $input.val() ),
+            isProblem = false;
+      
+        if ( num === num ) {
+          
+          if ( num < min ) {
+            num = min;
+          } else if ( num > max ) {
+            num = max;
+          }
+      
+          $slider.slider("value", num );
+            $input.val( num );
+
+        } else {
+      
+          alert( "should be a number" );
+      
+        }
+      
+      });
+      
+      $slider.on("slide", function(e,ui) {
+        $input.val( ui.values[0] );
+      
+      });
+
+        $input2 = $("#to")
+          min = 0,
+          max = 50;
+
+      $input2.on("change", function(e) {
+      
+        var num = parseFloat( $input2.val() ),
+            isProblem = false;
+      
+        if ( num === num ) {
+          
+          if ( num < min ) {
+            num = min;
+          } else if ( num > max ) {
+            num = max;
+          }
+      
+          $slider.slider("value", num );
+            $input2.val( num );
+      
+        } else {
+      
+          alert( "should be a number" );
+      
+        }
+      
+      });
+      
+      $slider.on("slide", function(e,ui) {
+        $input2.val( ui.values[1] );
+      
+      });
+
+
+
+
+
+
 
 }); // document.ready
